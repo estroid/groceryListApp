6fa1f3ed4194f640d8ac5142ddb75938b94c88e6
estroid <estroid@gmail.com>
2016-01-02 17:42:15 -0500

    add comments about user functionalities

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,22 @@
-dfdfdfd
+$(document).(function(){
+
+
+
+
+}
+
+// User can add items
+
+
+
+// User can check and uncheck items
+
+
+
+
+// User can permanently delete items
+
+
+
+// Optional: users can move items to proper food category?
+// Optional: HTML <input> autocomplete Attribute //
