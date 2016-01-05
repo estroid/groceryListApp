753895ff7eea2e3cdca29e8c4879e4c9c4644d8d
estroid <estroid@gmail.com>
2016-01-04 22:26:32 -0500

    add style

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,33 @@
-$(document).(function(){
-
-
-
-
-}
+$(document).ready(function(){
 
 // User can add items
 
-
+	var list = $('#listOfItems');
+	$('form').submit(function(event){
+		event.preventDefault();
+		var newItem = $(this).find('input').val();
+		console.log(newItem);
+		list.prepend('<li>'+newItem+' <span>X</span></li>');
+	});
 
 // User can check and uncheck items
 
+list.on('dblclick','li',itemClick);
 
+function itemClick(event){
+    event.stopPropagation();
+    $(this).toggleClass('completed');
+}
 
 
 // User can permanently delete items
 
+list.on('click','li span',function(){
+    $(this).parent().remove();
+});
 
 
 // Optional: users can move items to proper food category?
 // Optional: HTML <input> autocomplete Attribute //
+
+});
