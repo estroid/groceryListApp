67ab4a99e81377ccc4fbc982af9a67ff9892f03b
estroid <estroid@gmail.com>
2016-01-11 10:15:28 -0500

    fix css

--- js/custom.js
+++ js/custom.js
@@ -8,25 +8,31 @@ $(document).ready(function(){
 
  // inserts value of newItem variable into html with the .append()
 
-    $('#list').append("<li class='item'><label><input type='checkbox' name='new-item'>" + newItem + "</label><br></li><button name='button' id='deleteButton' class='delete'>X</button>"); // Appends (or adds after) html list item objects to the <ul> with the id of list
+    $('#list').append("<li class='item'><label><input type='checkbox' name='new-item' class='checkbox'>" + newItem + "</label> <button name='button' class='delete'>X</button></li>"); // Appends (or adds after) html list item objects to the <ul> with the id of list
     $("#item").val(""); // Since we prevented default behavior above, we need to reset the input field so that the input field stays clear after each submission
   });
 
 // User can check/strikethrough and uncheck items
-	$('#list').on('click', 'li', function(){ // .on(events[,selector],handler) method will listen for a click event on a particular list item and will run the function
+	$('#list').on('click', '.checkbox', function(){ // .on(events[,selector],handler) method will listen for a click event on a particular list item and will run the function
 		console.log('hello'); //should see "hello" when I check/uncheck an item
-		$(this).toggleClass('completed'); // Swaps out .completed css either to line-through or no line-through when the list item is clicked
+		$(this).closest('li').toggleClass('completed'); // Swaps out .completed css either to line-through or no line-through when the list item is clicked
 	});
 
 // User can permanently delete items
 
-	$('#list').on('click', '#deleteButton', function() {
+	$('#list').on('click', '.delete', function() {
 		console.log('whattup');
+
+		// (this) removes the whole list element
+		$(this).closest('li').remove(); // .closest has to be an element in which delete is inside of
+
+		// (this) removes the delete button that was clicked by the user
+		// $(this).remove();
+
+		// This deletes all the buttons
+		// $('.deleteButton').remove();
+
 		});
-
-	//If the button is clicked, then user can select to delete it
-
-	// $(.deleteButton).remove();
 
 
 // Optional: users can move items to proper food category?
